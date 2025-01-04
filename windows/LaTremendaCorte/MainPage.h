407986cd2c347687aa708fb22462cf255325b2b0
Diego Vasquez <elsrjuez@live.com>
2025-01-04 14:15:18 +0100

    init

---
+++ windows/LaTremendaCorte/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::LaTremendaCorte::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::LaTremendaCorte::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
